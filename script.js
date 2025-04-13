5475b4b4479ca0fa7708c1108bebbbccaa3c3a83
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2025-04-13 13:15:26 -0300

    wpp btn added
    still need href and customization

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const btnTema = document.getElementById('btnTema');
+document.documentElement.setAttribute("data-tema", window.matchMedia('(prefers-color-scheme: light)').matches ? "claro" : "escuro");
+btnTema.querySelector('img').src=`assets/icons/${window.matchMedia('(prefers-color-scheme: light)').matches ? "escuro" : "claro"}.svg`;
+
 btnTema.addEventListener('click',()=>{
     const tema = document.documentElement.getAttribute("data-tema");
     btnTema.querySelector('img').src=`assets/icons/${tema}.svg`;
