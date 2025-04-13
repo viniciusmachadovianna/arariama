131451a3a1ec6adcff94ee0fb6cb5c3f78ae0d5d
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2025-04-13 12:53:10 -0300

    ajuste de tema e add btn whatsapp

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnTema = document.getElementById('btnTema');
 btnTema.addEventListener('click',()=>{
-    console.log('clicked');
-    
+    const tema = document.documentElement.getAttribute("data-tema");
+    btnTema.querySelector('img').src=`assets/icons/${tema}.svg`;
+    let proxTema=tema==="claro"?"escuro":"claro";
+    document.documentElement.setAttribute("data-tema", proxTema);
 })
